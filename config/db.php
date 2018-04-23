599cd891ee9a2430bf823e4abbd33cda7433667e
MasterKKM <kar-gi-kar@mail.ru>
2018-04-23 17:51:02 +0300

    Настройка окружения.

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=192.168.100.2;dbname=yiibase',
+    'username' => 'master',
+    'password' => 'password',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
