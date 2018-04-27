d9b5fe4cb64692de2e5697d3fdcf91043d8dcd7f
MasterKKM <kar-gi-kar@mail.ru>
2018-04-27 12:10:38 +0300

    Подключен Fancybox.

--- views/site/_list.php
+++ views/site/_list.php
@@ -4,7 +4,7 @@
 use yii\helpers\Url;
 ?>
 <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-    <a href="#" class="thumbnail">
+    <a href="<?=Url::to(['/image/load', 'name' => 'image_' . $model->id . '.png']) ?>" "rel" = "fancybox" class="thumbnail">
         <img src="<?=Url::to(['/image/load', 'name' => 'image_' . $model->id . '_1.png']) ?>" />
     </a>
 </div>
--- views/site/index.php
+++ views/site/index.php
@@ -1,36 +1,43 @@
 <?php
 
+use yii\helpers\Html;
+use newerton\fancybox\FancyBox;
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = 'My Yii Application';
 ?>
-<div class="site-index">
+    <div class="site-index">
 
-    <div class="jumbotron">
-        <h1>Галерея!</h1>
+        <div class="jumbotron">
+            <h1>Галерея!</h1>
 
-        <p class="lead">Это глалерея она показывается только зарегрстрированным пользователям!.</p>
+            <p class="lead">Это глалерея она показывается только зарегрстрированным пользователям!.</p>
 
-    </div>
+        </div>
 
-    <div class="body-content">
+        <div class="body-content">
 
-        <div class="row">
-            <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                </div>
+                <?php
+                $dataProvider = new \yii\data\ActiveDataProvider([
+                    'query' => \app\models\Image::find(),
+                    'pagination' => [
+                        'pageSize' => 6,
+                    ],
+                ]);
+                echo \yii\widgets\ListView::widget([
+                    'dataProvider' => $dataProvider,
+                    'itemView' => '_list',
+                ]);
+                ?>
             </div>
-            <?php
-            $dataProvider = new \yii\data\ActiveDataProvider([
-                'query' => \app\models\Image::find(),
-                'pagination' => [
-                    'pageSize' => 6,
-                ],
-            ]);
-            echo \yii\widgets\ListView::widget([
-                'dataProvider' => $dataProvider,
-                'itemView' => '_list',
-            ]);
-            ?>
         </div>
     </div>
-</div>
+<?php
+echo FancyBox::widget([
+    'target' => '.thumbnail',
+]);
+?>
