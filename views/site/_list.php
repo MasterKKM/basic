9654bc377a82da46f5f9a1243cdca24eeed832bc
MasterKKM <kar-gi-kar@mail.ru>
2018-04-27 12:24:05 +0300

    Настроен вид картинок в галерее.

--- views/site/_list.php
+++ views/site/_list.php
@@ -4,7 +4,10 @@
 use yii\helpers\Url;
 ?>
 <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-    <a href="<?=Url::to(['/image/load', 'name' => 'image_' . $model->id . '.png']) ?>" "rel" = "fancybox" class="thumbnail">
+    <a href="<?=Url::to(['/image/load', 'name' => 'image_' . $model->id . '.png']) ?>" class="thumbnail">
         <img src="<?=Url::to(['/image/load', 'name' => 'image_' . $model->id . '_1.png']) ?>" />
     </a>
+    <div style="height: 60px;margin-top: -15px">
+        <?= $model->text ?>
+    </div>
 </div>
