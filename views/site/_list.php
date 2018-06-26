a59c93e0b344fb6e9a8e6942d35483b33b62595f
MasterKKM <kar-gi-kar@mail.ru>
2018-06-26 09:18:56 +0000

    В галерее добавлен вывод даты и ссылки на скачивание фото.

--- views/site/_list.php
+++ views/site/_list.php
@@ -2,11 +2,16 @@
 /* @var $model app\models\Image */
 
 use yii\helpers\Url;
+
 ?>
 <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
-    <a href="<?=Url::to(['/image/load', 'name' => 'image_' . $model->id . '.png']) ?>" class="thumbnail">
-        <img src="<?=Url::to(['/image/load', 'name' => 'image_' . $model->id . '_1.png']) ?>" />
+    <a href="<?= Url::to(['/image/load', 'name' => 'image_' . $model->id . '.png']) ?>" class="thumbnail">
+        <img src="<?= Url::to(['/image/load', 'name' => 'image_' . $model->id . '_1.png']) ?>"/>
     </a>
+    <div class="bg-primary"
+         style="position: absolute; left: 20px; top: 5px; width: 85px;"><?= $model->event_date ?></div>
+    <div class="bg-primary"
+         style="position: absolute; right: 20px; top: 5px; width: 85px;"><a href="<?= Url::to(['/image/load', 'name' => 'image_' . $model->id . '.png']) ?>" class="btn btn-success btn-xs btn-block">Скачать</a></div>
     <div style="height: 60px;margin-top: -15px">
         <?= $model->text ?>
     </div>
