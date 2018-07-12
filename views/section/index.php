ee68eefa10a862d92fa82f527532fdacf99bb110
MasterKKM <kar-gi-kar@mail.ru>
2018-07-12 10:03:11 +0000

    GRUD для управления разделами.

---
+++ views/section/index.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Разделы';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="section-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Section', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            'id',
+            'name',
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
